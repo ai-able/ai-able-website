967acc922a928da4727dc54fa7dfa4b16d4bdd40
Hollin Wakefield <45081384+hollinwakefield@users.noreply.github.com>
2023-03-10 23:02:23 -0500

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import video from "./640x640.mp4";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video
+        autoplay=""
+        loop="loop"
+        id="video-background-7608"
+        muted="muted"
+        plays-inline=""
+        poster=""
+      >
+        <source src={video} type="video/mp4" />
+      </video>
+
+      <h1 >ai-able</h1>
+      <p>Build the habit of using AI</p>
+      <a
+        className="App-link"
+        href="https://chrome.google.com/webstore/category/extensions"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Install the Chrome extension
+      </a>
     </div>
   );
 }
